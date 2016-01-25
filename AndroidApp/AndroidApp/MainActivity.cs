887bd2a1169293e0ca2c11d0a989cad04c9575d6
ASladnev <ASladnev@gmail.com>
2016-01-25 16:49:56 +0300

    1111

---
+++ AndroidApp/AndroidApp/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidApp
+{
+  [Activity(Label = "Приложение на Xamarin", MainLauncher = true, Icon = "@drawable/icon")]
+  public class MainActivity : Activity
+  {
+    int count = 1;
+
+    protected override void OnCreate(Bundle bundle)
+    {
+      base.OnCreate(bundle);
+
+      // Set our view from the "main" layout resource
+      SetContentView(Resource.Layout.Main);
+
+      // Get our button from the layout resource,
+      // and attach an event to it
+      {
+        var button = FindViewById<Button>(Resource.Id.MyButton);
+        button.Click += (s, e) => button.Text = $"{count++} Кликни меня";
+        //button.TextColors.
+      }
+ 
+      FindViewById<Button>(Resource.Id.buttonQuit).Click += (s, e) => Process.KillProcess(Process.MyPid());
+      
+
+    }
+  }
+}
+
